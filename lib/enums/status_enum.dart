19c9f1782e9c177cca31ea368992a6a87d75b0b2
phiphiphi2k1 <philonnni@gmail.com>
2024-05-29 18:22:16 +0700

    update: status

--- lib/enums/status_enum.dart
+++ lib/enums/status_enum.dart
@@ -9,9 +9,37 @@ enum Status {
   final String message;
 }
 
-enum OrderStatus {
-  preparing(3, 'Chờ chuẩn bị'),
+enum ExchangeGiftStatus {
+  preparing(1, 'Đang chuẩn bị'),
   delivering(4, 'Chờ giao hàng'),
+  completed(5, 'Hoàn thành'),
+  cancelled(6, 'Đã hủy');
+
+  const ExchangeGiftStatus(this.code, this.message);
+
+  final int code;
+  final String message;
+
+  static ExchangeGiftStatus fromInt(int code) {
+    switch (code) {
+      case 1:
+        return ExchangeGiftStatus.preparing;
+      case 4:
+        return ExchangeGiftStatus.delivering;
+      case 5:
+        return ExchangeGiftStatus.completed;
+      case 6:
+        return ExchangeGiftStatus.cancelled;
+      default:
+        return ExchangeGiftStatus.preparing;
+    }
+  }
+}
+
+enum OrderStatus {
+  preparing(2, 'Đang chuẩn bị'),
+  cooking(3, 'Đang chế biến'),
+  delivering(4, 'Đang giao hàng'),
   completed(5, 'Hoàn thành'),
   cancelled(6, 'Đã hủy');
 
@@ -22,13 +50,17 @@ enum OrderStatus {
 
   static OrderStatus fromInt(int code) {
     switch (code) {
-      case 3:
+      case 2:
         return OrderStatus.preparing;
+      case 3:
+        return OrderStatus.cooking;
       case 4:
         return OrderStatus.delivering;
       case 5:
         return OrderStatus.completed;
       case 6:
+        return OrderStatus.cancelled;
+      case 7:
         return OrderStatus.cancelled;
       default:
         return OrderStatus.preparing;
